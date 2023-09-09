5b795e144f9a5b06c9cbc9ff59e326f3d327d579
Matthew McNee <matthew.mcnee@student.manchester.ac.uk>
2023-09-09 12:06:02 +0100

    Working hover on links

--- main.js
+++ main.js
@@ -42,6 +42,7 @@ gltfLoader.load('enterprise.glb', (gltf) => {
   // loadedModel.castShadow = true;
   // Add the loaded model to the scene
   scene.add(loadedModel);
+  createLights();
   animate();
 });
 
@@ -65,27 +66,118 @@ document.addEventListener('mousedown', (event) => {
     }
 });
 
-function makeTextAt(font, text, position){
+
+// Add an array to store loaded models
+const hoverModels = [];
+const hoverText = [];
+const isObjectHoveredArray = Array(hoverModels.length).fill(false);
+// var curHovered = -1;
+
+// Listen for mousemove events
+const canvas = document.querySelector('#bg');
+document.addEventListener('mousemove', (event) => {
+  // Calculate mouse position in normalized device coordinates
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  // Iterate through the loaded models
+  for (let i = 0; i < hoverModels.length; i++) {
+    // Raycast from the camera for each model
+    raycaster.setFromCamera(mouse, camera);
+    
+    // Check if the ray intersects with the current model
+    const intersects = raycaster.intersectObject(hoverModels[i]);
+    if (intersects.length > 0) {
+      if (!isObjectHoveredArray[i]) {
+          isObjectHoveredArray[i] = true;
+          // Perform actions when the object is hovered over
+          console.log(`Model ${i + 1} Hovered!`);
+          hoverText[i].material.color.set(0xbbbbff);
+          canvas.style.cursor = "pointer";
+      }
+    } else {
+      if (isObjectHoveredArray[i]) {
+          isObjectHoveredArray[i] = false;
+          // Perform actions when the object is no longer hovered over
+          console.log(`Model ${i + 1} No Longer Hovered!`);
+          hoverText[i].material.color.set(0xeeeeee);
+          canvas.style.cursor = "default";
+      }
+    }
+  }
+});
+
+
+
+const lights = [];
+function createLights() {
+  // Create a point light source
+  const center = new THREE.Vector3(0, 0, 0);
+  const numLights = 24;
+  const lightDistance = 40;
+  for (let i = 0; i < numLights; i++) {
+    const phi = Math.acos(-1 + (2 * i) / numLights); // Angle from top to bottom
+    const theta = Math.sqrt(numLights * Math.PI * 2) * phi; // Angle around the sphere
+
+    const light = new THREE.PointLight(0x606060, 1000);
+
+    // Calculate the position using spherical coordinates
+    light.position.x = center.x + lightDistance * Math.cos(theta) * Math.sin(phi);
+    light.position.y = center.y + lightDistance * Math.sin(theta) * Math.sin(phi);
+    light.position.z = center.z + lightDistance * Math.cos(phi);
+
+    scene.add(light);
+
+    // Make each light point at the cube
+    light.target = loadedModel;
+    lights.push(light);
+  }
+}
+
+function makeTextAt(font, text, position, cubeColor) {
+  // Create a material for the cube
+  const cubeMaterial = new THREE.MeshBasicMaterial({ color: cubeColor, transparent: true, opacity: 0});
+
+  const cubeGeometry = new THREE.BoxGeometry(text.length * 2, 4, 0.2);
+
+  // Create a mesh for the cube
+  const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+  // Create a mesh for the text
   const textGeometry = new TextGeometry(text, {
     size: 2,
     height: 0.2,
     font: font,
   });
-  const textMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const textMaterial = new THREE.MeshBasicMaterial({ color: 0xeeeeee });
   const textMesh = new THREE.Mesh(textGeometry, textMaterial);
 
+  // Create a group to hold both the cube and text
   const group = new THREE.Group();
 
+  // Set the position of the text and cube
   textMesh.position.set(-5, 0, 0);
-  // Add the text to the group
-  group.add(textMesh);
-  // Set the group's position to (0, 40, 0)
-  group.position.copy(position)
+  cubeMesh.position.set(0, 1, 0);
 
+  // Add the cube and text to the group
+  group.add(cubeMesh);
+  group.add(textMesh);
+
+  // Set the group's position
+  group.position.copy(position);
+
+  // Add the group to the scene
   scene.add(group);
 
-  textArray.push(group)
+  // Push the group and textMesh to your arrays
+  textArray.push(group);
+  hoverModels.push(group);
+  hoverText.push(textMesh);
+  console.log(hoverModels);
 }
+
+
+
 
 
 
@@ -118,9 +210,11 @@ renderer.render(scene, camera);
 const animate = () => {
   requestAnimationFrame(animate);
 
-  textArray[0].lookAt(camera.position);
-  textArray[1].rotation.copy(textArray[0].rotation);
-  textArray[2].rotation.copy(textArray[0].rotation);
+  if (textArray.length > 0) {
+    textArray[0].lookAt(camera.position);
+    textArray[1].rotation.copy(textArray[0].rotation);
+    textArray[2].rotation.copy(textArray[0].rotation);
+  }
 
   renderer.render(scene, camera);
 };
